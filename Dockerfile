20bd38357f50f028c901b597fb3f4a2b785eb763
anddreyko <anddreyko@gmail.com>
2023-03-17 09:38:10 +0300

    feat(arc): installing Slim

--- Dockerfile
+++ Dockerfile
@@ -5,10 +5,15 @@ COPY .docker/php-fpm/conf.d /usr/local/etc/php/conf.d
 COPY . .
 
 # Image config
-ENV SKIP_COMPOSER 1
+ENV SKIP_COMPOSER 0
 ENV WEBROOT /var/www/html/web
-ENV PHP_ERRORS_STDERR 1
+ENV PHP_ERRORS_STDERR 0
+ENV ERRORS 1
 ENV RUN_SCRIPTS 1
 ENV REAL_IP_HEADER 1
+
+ENV COMPOSER_ALLOW_SUPERUSER 1
+
+RUN composer install --no-dev && rm -rf /root/.composer/cache
 
 WORKDIR /app
--- web/index.php
+++ web/index.php
@@ -2,6 +2,21 @@
 
 declare(strict_types=1);
 
-header('Content-Type: application/json');
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use Slim\Factory\AppFactory;
 
-echo '{"content": "Hello world!"}';
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = AppFactory::create();
+
+$app->get(
+    '/',
+    function (RequestInterface $request, ResponseInterface $response) {
+        $response->getBody()->write('{"content": "Hello world!"}');
+
+        return $response->withHeader('Content-Type', 'application/json');
+    }
+);
+
+$app->run();
