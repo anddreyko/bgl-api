36e9b61919aa31f0bc4b2ba9763daa0849be6c83
anddreyko <anddreyko@gmail.com>
2023-04-24 22:55:00 +0300

    refactor(arc): move console commands to directory of commands

--- config/base/console.php
+++ config/base/console.php
@@ -2,20 +2,20 @@
 
 declare(strict_types=1);
 
-use App\Console\HelloCommand;
-use Doctrine\ORM\Tools\Console\Command\ValidateSchemaCommand;
+use Commands\HelloCommand;
 
 return [
     'console' => [
         'commands' => [
-            ValidateSchemaCommand::class,
-            HelloCommand::class,
+            Doctrine\ORM\Tools\Console\Command\ValidateSchemaCommand::class,
 
             Doctrine\Migrations\Tools\Console\Command\ExecuteCommand::class,
             Doctrine\Migrations\Tools\Console\Command\ListCommand::class,
             Doctrine\Migrations\Tools\Console\Command\MigrateCommand::class,
             Doctrine\Migrations\Tools\Console\Command\StatusCommand::class,
             Doctrine\Migrations\Tools\Console\Command\UpToDateCommand::class,
+
+            HelloCommand::class,
         ],
     ],
 ];
