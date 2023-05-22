f14e17e40da933013ef341d032fa446a4d2ebf0f
anddreyko <anddreyko@gmail.com>
2023-05-22 15:19:39 +0300

    refactor(core): move tokenizer service

--- tests/Unit/Core/Tokens/Services/TokenizerServiceTest.php
+++ tests/Unit/Core/Tokens/Services/TokenizerServiceTest.php
@@ -2,20 +2,20 @@
 
 declare(strict_types=1);
 
-namespace Tests\Unit\Auth\Helpers;
+namespace Tests\Unit\Core\Tokens\Services;
 
-use App\Auth\Helpers\TokenizerHelper;
+use App\Core\Tokens\Services\TokenizerService;
 use Codeception\Test\Unit;
 
 /**
- * @covers \App\Auth\Helpers\TokenizerHelper
+ * @covers \App\Core\Tokens\Services\TokenizerService
  */
-class TokenizerHelperTest extends Unit
+class TokenizerServiceTest extends Unit
 {
-    public function testSuccess(): void
+    public function testGenerate(): void
     {
         $duration = new \DateInterval('PT1H');
-        $tokenizer = new TokenizerHelper($duration);
+        $tokenizer = new TokenizerService($duration);
 
         $now = new \DateTimeImmutable();
         $token = $tokenizer->generate($now);
