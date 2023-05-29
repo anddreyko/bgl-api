638cb88082a5cb5450f3ee858852e6de1eed5f52
anddreyko <anddreyko@gmail.com>
2023-05-30 01:48:49 +0300

    feat(http): add exception middleware

--- config/middleware.php
+++ config/middleware.php
@@ -5,5 +5,9 @@ declare(strict_types=1);
 use Slim\Middleware\ErrorMiddleware;
 
 return static function (Slim\App $app) {
+    $app->add(App\Core\Http\Middlewares\ExceptionMiddleware::class);
+    $app->add(App\Core\Http\Middlewares\EmptyFilesMiddleware::class);
+    $app->add(App\Core\Http\Middlewares\TrimMiddleware::class);
+    $app->addBodyParsingMiddleware();
     $app->add(ErrorMiddleware::class);
 };
