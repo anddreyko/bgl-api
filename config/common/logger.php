5623cf5b9df9262bd1854062eda654b07d413043
anddreyko <anddreyko@gmail.com>
2025-11-30 14:14:46 +0300

    chore(messages): inject envelope factory

--- config/common/logger.php
+++ config/common/logger.php
@@ -4,13 +4,21 @@ declare(strict_types=1);
 
 use Monolog\Handler\StreamHandler;
 use Monolog\Logger;
+use Psr\Container\ContainerInterface;
 use Psr\Log\LoggerInterface;
 
 return [
-    LoggerInterface::class => static function (): LoggerInterface {
+    LoggerInterface::class => static function (ContainerInterface $container): LoggerInterface {
+        /** @var array{file: string} $config */
+        $config = $container->get('logger');
+
         $logger = new Logger('application');
-        $logger->pushHandler(new StreamHandler(__DIR__ . '/../../var/.logs/application.log'));
+        $logger->pushHandler(new StreamHandler($config['file']));
 
         return $logger;
     },
+
+    'logger' => [
+        'file' => __DIR__ . '/../../var/.logs/application.log',
+    ],
 ];
--- config/dev/logger.php
+++ config/dev/logger.php
@@ -4,14 +4,18 @@ declare(strict_types=1);
 
 use Monolog\Handler\StreamHandler;
 use Monolog\Logger;
+use Psr\Container\ContainerInterface;
 use Psr\Log\LoggerInterface;
 use Psr\Log\NullLogger;
 
 return [
-    LoggerInterface::class => static function (): LoggerInterface {
+    LoggerInterface::class => static function (ContainerInterface $container): LoggerInterface {
         if ((bool)getenv('APP_DEBUG')) {
+            /** @var array{file: string} $config */
+            $config = $container->get('logger');
+
             $logger = new Logger('application');
-            $logger->pushHandler(new StreamHandler(__DIR__ . '/../../var/.logs/application.log'));
+            $logger->pushHandler(new StreamHandler($config['file']));
         } else {
             $logger = new NullLogger();
         }
