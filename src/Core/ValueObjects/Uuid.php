3f68a2633d0bda5424f3fdcd364c470e4d5efca9
anddreyko <anddreyko@gmail.com>
2025-12-14 17:33:50 +0300

    chore(value objects): email + uuid

---
+++ src/Core/ValueObjects/Uuid.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Bgl\Core\ValueObjects;
+
+final readonly class Uuid
+{
+    /**
+     * @param non-empty-string|null $value
+     */
+    public function __construct(
+        private ?string $value = null
+    ) {
+    }
+
+    public function isNull(): bool
+    {
+        return null === $this->value;
+    }
+
+    /**
+     * @return non-empty-string|null
+     */
+    public function getValue(): ?string
+    {
+        return $this->value;
+    }
+}
