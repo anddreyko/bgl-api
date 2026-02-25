42151b6fe7ddf923626776f1367ffc7ac9b953df
anddreyko <anddreyko@gmail.com>
2026-02-26 01:59:38 +0300

    fix(docker): add graceful shutdown and fix nginx health check

--- .docker/prod/entrypoint.sh
+++ .docker/prod/entrypoint.sh
@@ -1,5 +1,19 @@
 #!/bin/sh
 set -e
 
-php-fpm -D
-exec nginx -g 'daemon off;'
+cleanup() {
+    kill -TERM "$NGINX_PID" 2>/dev/null
+    kill -TERM "$FPM_PID" 2>/dev/null
+    wait "$NGINX_PID" "$FPM_PID" 2>/dev/null
+}
+
+trap cleanup TERM INT QUIT
+
+php-fpm &
+FPM_PID=$!
+
+nginx -g 'daemon off;' &
+NGINX_PID=$!
+
+wait -n
+cleanup
