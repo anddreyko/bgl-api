fc4cb0dee1fe1ef669fdfef4a2768cff196df3bd
anddreyko <anddreyko@gmail.com>
2026-03-22 14:37:15 +0300

    fix(auth): inherit AuthenticationException from DomainException

--- src/Core/Auth/AuthenticationException.php
+++ src/Core/Auth/AuthenticationException.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Bgl\Core\Auth;
 
-class AuthenticationException extends \RuntimeException
+class AuthenticationException extends \DomainException
 {
     protected $message = 'Authentication failed';
 }
